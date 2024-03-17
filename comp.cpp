20d125aea29c1f17f53e15ece7fa501c0a9f5160
Devansh Khetan <56186839+Devanshk9@users.noreply.github.com>
2024-03-17 20:44:25 +0530

    Create comp.cpp

---
+++ comp.cpp
@@ -0,0 +1,27 @@
+// DK09
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+#include <utility>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    typedef long long ll;
+    typedef vector<int> vi;
+
+    
+
+    return 0;
+}
